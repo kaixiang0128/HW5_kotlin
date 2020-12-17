b8c3ccba1fe967d88132e458ffedaebd97f34e9b
kaixiang0128 <t107360131@ntut.org.tw>
2020-12-17 19:06:45 +0800

    HW5_kotlin

---
+++ HW5_1/app/src/main/java/com/example/hw5_1/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.example.hw5_1
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val button = findViewById<Button>(R.id.button)
+        button.setOnClickListener {
+            val intent = Intent(this@MainActivity, MyService::class.java)
+            startService(intent)
+            Toast.makeText(this@MainActivity, "", Toast.LENGTH_SHORT).show()
+            finish()
+        }
+    }
+}
